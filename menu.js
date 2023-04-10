c17248f5c6dec740deddd6b260a3b75f8611e29a
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-10 23:39:27 +0300

    Add burger logic in menu.js for three states. Change header layout, add styles for mobile navigation.

--- menu.js
+++ menu.js
@@ -1,8 +1,26 @@
-function toggleMenu() {
-  var x = document.getElementById("menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
+const wrapper = document.getElementById('wrapper');
+const navItem = document.querySelectorAll('.nav-item');
+
+function toggleMenu () {
+  if (menu.className == 'menu-inactive') {
+    menu.className = ('menu-active');
   } else {
-    x.style.display = "block";
+    menu.className = ('menu-inactive');
   }
 }
+
+toggle.addEventListener("click", () => {
+  toggleMenu();
+ });
+
+ wrapper.addEventListener("click", () => {
+  if (menu.className == 'menu-active') {
+    menu.className = ('menu-inactive')};
+ })
+
+ navItem.forEach(navItem => navItem.addEventListener("click", () => {
+  toggleMenu();
+ }))
+
