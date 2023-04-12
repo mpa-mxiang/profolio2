283d7827bdd313aee7f472d586bfeedfcac0b340
Jiachun Xiang <max997123@gmail.com>
2023-04-12 13:36:14 -0400

    initialize js done

--- popup.js
+++ popup.js
@@ -9,7 +9,8 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
@@ -21,7 +22,22 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
+},
+  ,
+{
+  title: 'Multi Post Stories',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only
+                    five centuries, but also the leap into electronic typesetting, remaining essent`,
+  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  liveDemoLink: 'https://example.com',
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
@@ -33,7 +49,8 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
@@ -45,7 +62,8 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
@@ -57,7 +75,8 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
@@ -69,33 +88,57 @@ const data = [{
                     five centuries, but also the leap into electronic typesetting, remaining essent`,
   technologies: ['HTML', 'Bootstrap', 'Ruby'],
   liveDemoLink: 'https://example.com',
-  sourceCodeLink: 'https://github.com/example'
+  sourceCodeLink: 'https://github.com/example',
+  image: './img/work.png'
 }
 ];
 const modal = document.getElementById("popup-container");
-const btn = document.getElementById("popup-trigger");
+const btn = document.querySelectorAll("popup-trigger");
 const span = document.getElementsByClassName("close")[0];
-
+const work = document.getElementById("works");
 data.forEach(function (project) {
-  console.log('Title:', project.title);
-  console.log('Technologies:', project.technologies.join(', '));
-  console.log('Description:', project.description);
-  console.log('Live Demo Link:', project.liveDemoLink);
-  console.log('Source Code Link:', project.sourceCodeLink);
+  work.innerHTML += `
+    <img src="${project.image}" alt="Art Selection Project Image" />
+    <div class="div-text">
+        <h1 aria-label="Profesional Art Printing Data" class="hover">${project.title}</h1>
+        <p aria-label="A daily selection of privately personalized reads;
+        no accounts or sign-ups required. has been the
+        industry's standard" class="hover">
+        ${project.description}
+        </p>
+        <ul>
+        ${project.technologies}
+        </ul>
+    </div>
+    <button type="button" class="hover-btn" aria-label="press to see the project" id="${project.id}">See project</button>
+      `;
 });
 
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+for (let i = 0; i < openWorks.length; i += 1) {
+  btn[i].onclick = function () {
+    const body = document.querySelector('body');
+    body.classList.add('overflowH');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+            workspopup.innerHTML += `
+        <div>
+        <span class="close" >& times;</span >
+        <h1>${project[i].title}</h1>
+        <ul>${project[i].technologies}</ul>
+        <div class="popup-div">
+              <img>${project[i].image}</img>
+              <p>${project[i].description}</p>
+            </div>
+            <div class="btn-div">
+                <button type="button" aria-label="project live demo" tabindex="-13"><a href="${project[i].liveDemoLink}">See live<img src="./img/live-demo.png" alt="live demo icon" /></a></button>
+                <button type="button" aria-label="project source code" tabindex="-14"><a href="${project[i].sourceCodeLink}">See source<img src="./img/github-button.png" alt="github icon" /></a>/button>
+                </div>
+        </div>
+    `;
+    span.onclick = function () {
     modal.style.display = "none";
+    body.classList.remove('overflowH');
+    workspopup.innerHTML = '';
+  }
   }
 }
