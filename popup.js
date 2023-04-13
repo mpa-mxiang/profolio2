5a1360a786f638e51ad3882df6b7457e8f14856b
Jiachun Xiang <max997123@gmail.com>
2023-04-13 00:05:36 -0400

    fix click and style issues

--- popup.js
+++ popup.js
@@ -1,25 +1,20 @@
 
 const data = [{
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
@@ -27,65 +22,50 @@ const data = [{
   ,
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
 },
 {
   title: 'Multi Post Stories',
-  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text
-                    ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essent`,
-  technologies: ['HTML', 'Bootstrap', 'Ruby'],
+  technologies: '<li>HTML</li><li>Bootstrap</li><li>Ruby</li>',
+  description: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. has been the
+  industry's standard.`,
   liveDemoLink: 'https://example.com',
   sourceCodeLink: 'https://github.com/example',
   image: './img/work.png'
@@ -93,6 +73,7 @@ const data = [{
 ];
 
 const work = document.getElementById("projects");
+
 data.forEach(function (project) {
   work.innerHTML += `
   <div class="normal">
@@ -115,15 +96,12 @@ data.forEach(function (project) {
 const btn = document.querySelectorAll(".popup-trigger");
 const modal = document.getElementById("popup-container");
 
+
 for (let i = 0; i < btn.length; i += 1) {
   btn[i].addEventListener('click', () => {
-    const body = document.querySelector('body');
-
-    body.classList.add('overflowH');
 
 
-    modal.innerHTML += `
-        <div>
+    modal.innerHTML = `
         <span class="close" ><img src="./img/close-icon.png" /></span >
         <h1>${data[i].title}</h1>
         <ul>${data[i].technologies}</ul>
@@ -135,16 +113,15 @@ for (let i = 0; i < btn.length; i += 1) {
                 <button type="button" aria-label="project live demo" tabindex="-13"><a href="${data[i].liveDemoLink}">See live<img src="./img/live-demo.png" alt="live demo icon" /></a></button>
                 <button type="button" aria-label="project source code" tabindex="-14"><a href="${data[i].sourceCodeLink}">See source<img src="./img/github-button.png" alt="github icon" /></a>/button>
                 </div>
-        </div>
     `;
     const span = document.getElementsByClassName("close")[0];
 
-    modal.style.display = "block";
+    modal.style.display = "flex";
     span.onclick = function () {
       modal.style.display = "none";
-      body.classList.remove('overflowH');
-      workspopup.innerHTML = '';
+      modal.innerHTML = '';
     }
   
-  }, {once : true});
+  });
 }
+
