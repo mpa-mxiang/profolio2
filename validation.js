361f7c1ddd8f7cbc6f9fe9bac98d7c5cf2c22dda
Jiachun Xiang <max997123@gmail.com>
2023-04-13 00:16:58 -0400

    done validation function

--- validation.js
+++ validation.js
@@ -1,58 +1,15 @@
-// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the <small> element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
-}
+const emailInput = document.getElementById('email');
+const form = document.getElementById('contact-form');
+const validateMessage = document.getElementById('validate');
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
-
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
-
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
-
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, "ERROR: Please follow the proper email format, make sure your email is all lowercase!");
-	}
-	return true;
-}
-
-const form = document.querySelector("#contact-form");
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	Event.preventDefault();
-
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		form.submit();
-	}
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const email = emailInput.value.trim();
+  const isLowercase = email === email.toLowerCase();
+  if (!isLowercase) {
+    validateMessage.innerText = 'Please enter a lowercase email address';
+    validateMessage.style.color = 'red';
+    return;
+  }
+  form.submit();
 });
