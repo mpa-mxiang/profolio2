27a18ae2b73d9e9bcb5ce152a253179ac9ebadf8
Jiachun Xiang <max997123@gmail.com>
2023-04-14 12:32:32 -0400

    improved js

--- storage.js
+++ storage.js
@@ -1,39 +1,34 @@
-// Get the form elements
-const form = document.querySelector('form');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const commentsInput = document.getElementById('comments');
+document.addEventListener('DOMContentLoaded', () => {
+  // Get the form elements
+  const form = document.querySelector('form');
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const commentsInput = document.getElementById('comments');
 
-// Load the form data from local storage
-const formData = JSON.parse(localStorage.getItem('formData')) || {};
+  // Load the form data from local storage
+  const formData = JSON.parse(localStorage.getItem('formData')) || {};
 
-// Set the form values if they exist in local storage
-if (formData.name) {
-  nameInput.value = formData.name;
-}
+  // Set the form values if they exist in local storage
+  if (formData.name) {
+    nameInput.value = formData.name;
+  }
 
-if (formData.email) {
-  emailInput.value = formData.email;
-}
+  if (formData.email) {
+    emailInput.value = formData.email;
+  }
 
-if (formData.comments) {
-  commentsInput.value = formData.comments;
-}
+  if (formData.comments) {
+    commentsInput.value = formData.comments;
+  }
 
-// Save the form data to local storage whenever the user changes an input field
-form.addEventListener('input', () => {
-  console.log("form input");
-  formData.name = nameInput.value;
-  formData.email = emailInput.value;
-  formData.comments = commentsInput.value;
-  
-  localStorage.setItem('formData', JSON.stringify(formData));
+  // Save the form data to local storage whenever the user changes an input field
+  form.addEventListener('input', () => {
+    formData.name = nameInput.value;
+    formData.email = emailInput.value;
+    formData.comments = commentsInput.value;
+
+    localStorage.setItem('formData', JSON.stringify(formData));
+
+    console.log(formData);
+  });
 });
-
-const savedData = JSON.parse(localStorage.getItem('formData'));
-if (savedData) {
-  console.log("saved input");
-  document.getElementById('name').value = savedData.name;
-  document.getElementById('email').value = savedData.email;
-  document.getElementById('comments').value = savedData.comments;
-}
