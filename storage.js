e1130d9e2248af29b0d0ab73e74cd661a125dee7
Jiachun Xiang <max997123@gmail.com>
2023-04-14 14:02:19 -0400

    install hint

--- storage.js
+++ storage.js
@@ -1,34 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Get the form elements
-  const form = document.querySelector('form');
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const commentsInput = document.getElementById('comments');
+const form = document.querySelectorAll('#contact-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const commentsInput = document.getElementById('comments');
 
-  // Load the form data from local storage
-  const formData = JSON.parse(localStorage.getItem('formData')) || {};
+const formData = JSON.parse(localStorage.getItem('formData')) || {};
 
-  // Set the form values if they exist in local storage
-  if (formData.name) {
-    nameInput.value = formData.name;
-  }
+if (formData.name) {
+  nameInput.value = formData.name;
+}
 
-  if (formData.email) {
-    emailInput.value = formData.email;
-  }
+if (formData.email) {
+  emailInput.value = formData.email;
+}
 
-  if (formData.comments) {
-    commentsInput.value = formData.comments;
-  }
+if (formData.comments) {
+  commentsInput.value = formData.comments;
+}
 
-  // Save the form data to local storage whenever the user changes an input field
-  form.addEventListener('input', () => {
-    formData.name = nameInput.value;
-    formData.email = emailInput.value;
-    formData.comments = commentsInput.value;
+form.addEventListener('input', (event) => {
+  console.log('Input event triggered!');
+  formData[event.target.name] = event.target.value;
 
+  try {
     localStorage.setItem('formData', JSON.stringify(formData));
-
-    console.log(formData);
-  });
+  } catch (e) {
+    console.error('Error saving data to localStorage:', e);
+  }
 });
+
+const savedData = JSON.parse(localStorage.getItem('formData'));
+
+if (savedData) {
+  nameInput.value = savedData.name;
+  emailInput.value = savedData.email;
+  commentsInput.value = savedData.comments;
+}
