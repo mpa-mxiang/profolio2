c5c7c36d29bb751fab8c33e81d363d979714d510
Jiachun Xiang <max997123@gmail.com>
2023-04-14 11:47:02 -0400

    modify and add desktop font size for message

--- validation.js
+++ validation.js
@@ -1,19 +1,15 @@
 const emailInput = document.getElementById('email');
 const form = document.getElementById('contact-form');
 const validateMessage = document.getElementById('validate');
-function validate() {
-  const email = emailInput.value.trim();
-  const isLowercase = email.toLowerCase();
-  if (email !== isLowercase) {
-    validateMessage.innerText = 'Please enter a lowercase email address';
-    validateMessage.style.color = 'red';
-    return false;
-  }
-  return true;
-}
 
 form.addEventListener('submit', (event) => {
-  if (validate() === false) {
-    event.preventDefault();
+  event.preventDefault();
+  const email = emailInput.value.trim();
+  const isLowercase = email === email.toLowerCase();
+  if (!isLowercase) {
+    validateMessage.innerText = 'Please enter a lowercase email address';
+    validateMessage.style.color = 'red';
+    return;
   }
+  form.submit();
 });
