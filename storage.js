985ad57cf2af658c4ce4d353ef4da65fcbe06b23
Jiachun Xiang <max997123@gmail.com>
2023-04-23 22:09:38 -0400

    fix storage error with naming

--- storage.js
+++ storage.js
@@ -1,28 +1,29 @@
 const typedName = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+const typedEmail = document.querySelector('#email');
 const messageInput = document.querySelector('#comments');
-const form = document.getElementById('contact-form');
+const forms = document.getElementById('contact-form');
 const formData = JSON.parse(localStorage.getItem('formData')) || {};
 const savedData = JSON.parse(localStorage.getItem('formData'));
+console.log(formData)
 if (formData.name) {
   typedName.value = formData.name;
 }
 if (formData.email) {
-  emailInput.value = formData.email;
+  typedEmail.value = formData.email;
 }
 if (formData.comments) {
   messageInput.value = formData.message;
 }
-form.addEventListener('input', (event) => {
+forms.addEventListener('input', (event) => {
   formData[event.target.name] = event.target.value;
   localStorage.setItem('formData', JSON.stringify(formData));
 });
 if (savedData) {
-  emailInput.value = savedData.email;
+  typedEmail.value = savedData.email;
   messageInput.value = savedData.message;
   typedName.value = savedData.name;
 }
 // error is here
-form.addEventListener('submit', () => {
+forms.addEventListener('submit', () => {
   localStorage.clear();
 });
